a6678f58738fbd51b829c8ab14006d745d160036
Marstoj-LWSD <s-mstojkovicortega@Lwsd.org>
2024-05-12 15:23:47 -0700

    Added steel as item

--- src/main/java/net/marstoj/testmod/TestMod.java
+++ src/main/java/net/marstoj/testmod/TestMod.java
@@ -2,6 +2,7 @@ package net.marstoj.testmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.marstoj.testmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +12,7 @@ public class TestMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModItems.registerModItems();
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/net/marstoj/testmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.marstoj.testmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.marstoj.testmod.TestMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item STEEL = registerItem("steel", new Item(new Item.Settings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(STEEL);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Items for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
