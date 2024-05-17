a41a750480d3a653c7a42badce929d3221a8da65
Marstoj-LWSD <s-mstojkovicortega@Lwsd.org>
2024-05-16 20:50:08 -0700

    Added tools "Steel Pickaxe" and "Steel Axe"

--- src/main/java/net/marstoj/testmod/item/ModItems.java
+++ src/main/java/net/marstoj/testmod/item/ModItems.java
@@ -3,8 +3,10 @@ package net.marstoj.testmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.marstoj.testmod.TestMod;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,12 +16,20 @@ public class ModItems {
     public static final Item RAW_STEEL = registerItem("raw_steel", new Item(new Item.Settings()));
     public static final Item STEEL_SHEET = registerItem("steel_sheet", new Item(new Item.Settings()));
     public static final Item STEEL_SCRAP = registerItem("steel_scrap", new Item(new Item.Settings()));
-// TODO add steel pickaxe, axe
+    public static final Item STEEL_PICKAXE = registerItem("steel_pickaxe", new PickaxeItem(ModToolMaterial.STEEL, new Item.Settings().maxCount(1)));
+    public static final Item STEEL_AXE = registerItem("steel_axe", new AxeItem(ModToolMaterial.STEEL, new Item.Settings().maxCount(1)));
+
+
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(STEEL_INGOT);
         entries.add(RAW_STEEL);
         entries.add(STEEL_SHEET);
         entries.add(STEEL_SCRAP);
+    }
+
+    private static void addItemsToToolItemGroup(FabricItemGroupEntries entries) {
+        entries.add(STEEL_PICKAXE);
+        entries.add(STEEL_AXE);
     }
 
     private static Item registerItem(String name, Item item) {
@@ -30,5 +40,6 @@ public class ModItems {
         TestMod.LOGGER.info("Registering Mod Items for " + TestMod.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(ModItems::addItemsToToolItemGroup);
     }
 }
